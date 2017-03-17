1b4f6e5a528bb26b0d6b216c95f5379163d39a0f
thijsvb <thijsvb@gmail.com>
2017-03-17 15:25:38 +0100

    added Unity project

---
+++ App/VirtueleExpositie/Assets/Scripts/CheckButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    #region MONOBEHAVIOUR_METHODS
+
+    void Update()
+    {
+#if UNITY_ANDROID
+        // On Android, the Back button is mapped to the Esc key
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+#endif
+    }
+    #endregion // MONOBEHAVIOUR_METHODS
+}
---
+++ App/VirtueleExpositie/Assets/Scripts/LaadExpositie.cs
@@ -0,0 +1,51 @@
+﻿/*============================================================================== 
+Copyright (c) 2015-2016 PTC Inc. All Rights Reserved.
+Vuforia is a trademark of PTC Inc., registered in the United States and other 
+countries.   
+==============================================================================*/
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using Vuforia;
+
+public class LaadExpositie : MonoBehaviour
+{
+
+    #region PUBLIC_METHODS
+    public void laadAR()
+    {
+        ModeConfig.isFullScreenMode = true;
+        LoadNextScene();
+    }
+
+    public void laadVR()
+    {
+        ModeConfig.isFullScreenMode = false;
+        LoadNextScene();
+    }
+    #endregion // PUBLIC_METHODS
+
+
+    #region PRIVATE_METHODS
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene("Expositie");
+    }
+    #endregion //PRIVATE_METHODS
+
+
+    #region MONOBEHAVIOUR_METHODS
+
+    void Update()
+    {
+#if UNITY_ANDROID
+        // On Android, the Back button is mapped to the Esc key
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            // Exit app
+            Application.Quit();
+        }
+#endif
+    }
+    #endregion // MONOBEHAVIOUR_METHODS
+}
